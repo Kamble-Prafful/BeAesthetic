f18fe0a52da65a92baadabc112032c839ecaf383
Mysterious <73134414+cloutop@users.noreply.github.com>
2022-01-20 13:35:07 +0530

    initial commit

---
+++ Js files/script.js
@@ -0,0 +1,74 @@
+//* Menu Button for 768px width
+let menu = document.getElementById("menu-btn");
+let navbar = document.querySelector(".header .navbar");
+
+menu.onclick=()=>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
+
+window.onscroll=()=>{
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay:5000,
+        disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay:4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3
+        },
+    },
+});
